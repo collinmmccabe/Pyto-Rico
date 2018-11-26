10e05032963beccc84fcdaa7628a9bbf30714f60
Collin McCabe <collinmichaelmccabe@gmail.com>
2018-11-25 22:28:54 -0500

    pythonify market class pseudocode

--- market.py
+++ market.py
@@ -1,47 +1,122 @@
-Class Market
-    Class Buildings
-        Attr available_buildings = {'small indigo plant’: 3, 'small sugar plant’: 3, …}
-        Method building_bought(building)
-            If available_buildings[building] > 0, available_buildings[building] -= 1
-    Class Plantations
-        Attr face_down_pool = [‘corn’, ‘corn’, ‘sugar’, ‘coffee’, ‘coffee’, …]
-        Attr face_up_pool = [‘indigo’, ‘corn’, ‘coffee’, ‘coffee’] for 3 players
-        Attr discard_pool = (list)
-        Attr quarries (int)
-        Method draw_new_tiles()
-            Move all from face_up_pool to discard_pool, if length(face_down_pool) == 0, move all from discard_pool to face_down_pool (maybe deepcopy then wipe), while length of face_up_pool < (# players + 1): draw 1 random tile from face_down_pool, move that random tile to face)_up_pool, if length(face_down_pool) == 0, move all from discard_pool to face_down_pool
-    Class RoleOptions
-        Class Role(role_name)
-            Attr role_name = role_name
-            Attr stored_doubloons = (int)
-            Attr taken_flag = (bool)
-            Method choose_role(role)
-                If not RoleOptions[role].taken_flag, add RoleOptions[role].stored_doubloons to Player.doubloons, set RoleOptions[role].stored_doubloons to 0, set RoleOptions[role].taken_flag to T, do Phase[role_name].full_phase())
-    Class TradingPost
-        Attr trading_queue (list)
-        Method empty_queue()
-            For each resource in trading_queue, pop from trading_queue and add 1 of that type back to Supply
-    Class CaptainShips
-        Class Ship
-            Attr free_space = (int)
-            Attr used_space = (int)
-            Attr good_type = None (str or Enum)
-            Method load_ship(good, number)
-                If used_space == 0, or if good == Ship.good_type, then Ship.good_type = good, 
-    Class ColonistShip
-        Attr colonists_carried (int)
-        Attr colonists_supply (int)
-        Method new_voyage()
-            Move colonists from colonists_supply to colonists_carried equal to x = min(total number of unoccupied_squares in all players’ cities, number of players); if colonists_supply <= x, move the total available in colonist supply to carried, endgame_trigger = T)
-    Class Supply
-        Class Barrels
-            Attr corn (int)
-            Attr indigo (int)
-            Attr sugar (int)
-            Attr tobacco (int)
-            Attr coffee (int)
-        Attr vp (int)
-        Attr doubloons (int)
-        Method check_vp_supply()
-            If vp <= 0, endgame_trigger = T
-    Attr endgame_trigger (bool)
+import random
+
+class Market(colonists, vp, barrel_dict, plantation_list, quarries, building_dict, prospectors, ship_size_list):
+    def __init__(self):
+        self._endgame_trigger = False
+        self._buildings = Buildings(building_dict)
+        self._plantations = Plantations(plantation_list, quarries)
+        self._role_options = RoleOptions(prospectors)
+        self._trading_post = TradingPost()
+        self._captain_ships = CaptainShips(ship_size_list)
+        self._colonist_ship = ColonistShip(colonists)
+        self._supply = Supply(barrel_dict, vp)
+
+class Buildings(building_dict):
+    def __init__(self):
+        self._available_buildings = building_dict
+    def building_taken(self, building):
+        if self._available_buildings[building] > 0:
+            self._available_buildings[building] -= 1
+
+class Plantations(plantation_list, quarries):
+    def __init__(self, plantation_list, quarries):
+        self._face_down_pool = plantation_list
+        self._face_up_pool = []
+        self._discard_pool = []
+        self._quarries = quarries
+    def plantation_taken(self, plantation):
+        if plantation in self._face_up_pool:
+            self._face_up_pool.remove(plantation)
+            return plantation
+        return None
+    def quarry_taken(self):
+        if self._quarries > 0:
+            self.quarries -= 1
+            return 'quarry'
+        return None
+    def draw_new_tiles(self, num_players):
+        self._discard_pool += self._face_up_pool
+        self._face_up_pool.clear()
+        if len(self._face_down_pool) == 0:
+            self._face_down_pool += self._discard_pool
+            self._discard_pool.clear()
+        while len(self._face_up_pool) < (num_players + 1):
+            self._face_up_pool += self._face_down_pool.pop(random.randrange(len(self._face_down_pool)))
+            if len(self._face_down_pool) == 0:
+                self._face_down_pool += self._discard_pool
+                self._discard_pool.clear()
+        return None
+
+class RoleOptions(prospectors):
+    def __init__(self, prospectors):
+        self._role_list = [Role('mayor'), Role('captain'), Role('etc')]
+        self._role_list += (Role('prospector') * prospectors)
+        class Role(role_name):
+            def __init__(self):
+                self._role_name = role_name
+                self._stored_doubloons = 0
+                self._taken_flag = False
+    def choose_role(self, role):
+        if not self._role_list[role]._taken_flag:
+            Player.doubloons += self._role_list[role]._stored_doubloons
+            self._role_list[role]._stored_doubloons = 0
+            self._role_list[role]._.taken_flag = True
+            return Phase[role_name].full_phase()
+        return None
+
+class TradingPost():
+    def __init__(self):
+        self._trading_queue = []
+    def add_barrel(self, good_type):
+        if len(self._trading_queue) < 4:
+            self._trading_queue.append(good_type)
+            return good_type
+    def empty_queue(self):
+        while self._trading_queue:
+            Supply.Barrels[self._trading_queue.pop()] += 1
+        return None
+
+class CaptainShips(ship_size_list):
+    def __init__(self, ship_size_list):
+        self._ship_list = []
+        for ship_size in ship_size_list:
+            self._ship_list.append(Ship(ship_size))
+        class Ship(size):
+            def __init__(self):
+                self._free_space = size
+                self._used_space = 0
+                self._good_type = None
+            def load_ship(self, good, number):
+                if (used_space == 0 or good == self._good_type) and number <= self._free_space:
+                    self._good_type = good
+                    self._free_space -= number
+                    self._used_space += number
+                    return number
+                return None
+            def unload_ship(self):
+                if self._free_space == 0:
+                    Supply.Barrels[self._good_type] += self._used_space
+                    self._free_space += self._used_space
+                    self._used_space = 0
+                return None
+
+class ColonistShip(colonists):
+    def __init__(self, colonists):
+        self._colonists_carried = 0
+        self._colonists_supply = colonists
+    def new_voyage(self, new_colonists):
+        if self._colonists_supply > new_colonists:
+            self._colonists_supply -= new_colonists
+            self._colonists_carried += new_colonists
+        else:
+            self._colonists_carried += self._colonists_supply
+            self._colonists_supply = 0
+            Market.endgame_trigger = True
+
+class Supply(barrel_dict, vp):
+    def __init__(self):
+        self._barrels = barrel_dict
+        self._vp = vp
+    def check_vp_supply(self):
+        if self._vp <= 0:
+            Market.endgame_trigger = True
