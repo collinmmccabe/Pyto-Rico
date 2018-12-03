e1dbac011432b5f9d72bb3c642d0a12cb13bc6b2
Collin McCabe <collinmichaelmccabe@gmail.com>
2018-12-03 00:05:58 -0500

    Update market, TODO is in market_flat_class

--- market.py
+++ market.py
@@ -1,5 +1,4 @@
 import random
-from setup_dict import setup_dict
 from building_lookup import building_lookup
 
 class DictRef(dict):
@@ -9,128 +8,109 @@ class DictRef(dict):
         self[key] = value
 
 class Market(object):
-    def __init__(self, setup_subdict):
+    def __init__(self, setup_subdict, num_players):
         self.endgame_trigger = False
-        self.buildings = self.Buildings(setup_subdict.building_dict)
-        self.plantations = self.Plantations(setup_subdict.plantation_dict, setup_subdict.quarries)
-        self.role_options = self.RoleOptions(setup_subdict.prospectors)
+        self.buildings = self.Buildings(setup_subdict['building_dict'])
+        self.plantations = self.Plantations(setup_subdict['plantation_dict'], 
+                                            setup_subdict['quarries'],
+                                            num_players)
+        #self.role_options = self.RoleOptions(setup_subdict.prospectors)
         self.trading_post = self.TradingPost()
-        self.captain_ships = self.CaptainShips(setup_subdict.ship_size_list)
-        self.colonist_ship = self.ColonistShip(setup_subdict.colonists)
-        self.supply = self.Supply(setup_subdict.barrel_dict, setup_subdict.vp)
+        #self.captain_ships = self.CaptainShips(setup_subdict.ship_size_list)
+        self.colonist_ship = self.ColonistShip(setup_subdict['colonists'], num_players)
+        self.supply = self.Supply(setup_subdict['barrel_dict'], setup_subdict['vp'])
 
     class Buildings(DictRef):
         def __init__(self, building_dict):
-            for building in building_dict.key():
-                setattr(self, building, { 'available': building_dict[building], 'info': self.Building(building) })
+            for building, count in building_dict.items():
+                setattr(self, building, { 'available': count, 'info': self.Building(building) })
 
         def building_taken(self, building):
-            if self._available_buildings[building] > 0:
-                self._available_buildings[building] -= 1
+            if self[building].available > 0:
+                self[building].available -= 1
 
         class Building(DictRef):
             def __init__(self, building_name):
-                setattr(self, 'properties', building_lookup[building_name])
+                for key, value in building_lookup[building_name].items():
+                    setattr(self, key, value)
+            
+            def __repr__(self):
+                return ("Name: {}\nDescription: {}\nBuilding Type: {}\nGood Processed: {}\nWorker Spaces: {}\nSize: {}"
+                        "\nCost: {}\nQuarries Allowed: {}\nVP: {}".format(self.name,
+                                                                          self.perk_description,
+                                                                          self.building_type,
+                                                                          self.good_processed,
+                                                                          self.free_worker_spaces,
+                                                                          self.size,
+                                                                          self.base_cost,
+                                                                          self.quarries_allowed,
+                                                                          self.vp_value))
 
-class Plantations(object):
-    def __init__(self, plantation_dict, quarries):
-        self._face_down_pool = plantation_dict
-        self._face_up_pool = []
-        self._discard_pool = []
-        self._quarries = quarries
-    def plantation_taken(self, plantation):
-        if plantation in self._face_up_pool:
-            self._face_up_pool.remove(plantation)
-            return plantation
-        return None
-    def quarry_taken(self):
-        if self._quarries > 0:
-            self._quarries -= 1
-            return 'quarry'
-        return None
-    def draw_new_tiles(self, num_players):
-        self._discard_pool += self._face_up_pool
-        self._face_up_pool.clear()
-        if len(self._face_down_pool) == 0:
-            self._face_down_pool += self._discard_pool
-            self._discard_pool.clear()
-        while len(self._face_up_pool) < (num_players + 1):
-            self._face_up_pool += self._face_down_pool.pop(random.randrange(len(self._face_down_pool)))
-            if len(self._face_down_pool) == 0:
-                self._face_down_pool += self._discard_pool
-                self._discard_pool.clear()
-        return None
+    class Plantations(object):
+        def __init__(self, plantation_dict, quarries, number_of_players):
+            self.number_of_players = number_of_players
+            self.face_down_pool = []
+            for plantation, number in plantation_dict.items():
+                self.face_down_pool += [plantation] * number
+            self.face_up_pool = []
+            self.discard_pool = []
+            self.quarries = quarries
+            self.draw_new_tiles()
+        def plantation_taken(self, plantation):
+            if plantation in self.face_up_pool:
+                self.face_up_pool.remove(plantation)
+                return plantation
+            return None
+        def quarry_taken(self):
+            if self.quarries > 0:
+                self.quarries -= 1
+                return 'quarry'
+            return None
+        def draw_new_tiles(self):
+            self.discard_pool += self.face_up_pool
+            self.face_up_pool.clear()
+            if len(self.face_down_pool) == 0:
+                self.face_down_pool += self.discard_pool
+                self.discard_pool.clear()
+            while len(self.face_up_pool) < (self.number_of_players + 1):
+                self.face_up_pool += [self.face_down_pool.pop(random.randrange(len(self.face_down_pool)))]
+                if len(self.face_down_pool) == 0:
+                    self.face_down_pool += self.discard_pool
+                    self.discard_pool.clear()
+            return None
 
-class RoleOptions(object):
-    def __init__(self, prospectors):
-        self._role_list = [Role('mayor'), Role('captain'), Role('etc')]
-        self._role_list += (Role('prospector') * prospectors)
-        class Role(object):
-            def __init__(self, role_name):
-                self._role_name = role_name
-                self._stored_doubloons = 0
-                self._taken_flag = False
-    def choose_role(self, role):
-        if not self._role_list[role]._taken_flag:
-            Player.doubloons += self._role_list[role]._stored_doubloons
-            self._role_list[role]._stored_doubloons = 0
-            self._role_list[role]._.taken_flag = True
-            return Phase[role_name].full_phase()
-        return None
+    class TradingPost():
+        def __init__(self):
+            self.trading_queue = []
+        def add_barrel(self, good_type):
+            if len(self.trading_queue) < 4:
+                self.trading_queue.append(good_type)
+                return good_type
 
-class TradingPost():
-    def __init__(self):
-        self._trading_queue = []
-    def add_barrel(self, good_type):
-        if len(self._trading_queue) < 4:
-            self._trading_queue.append(good_type)
-            return good_type
-    def empty_queue(self):
-        while self._trading_queue:
-            Supply.Barrels[self._trading_queue.pop()] += 1
-        return None
-
-class CaptainShips(object):
-    def __init__(self, ship_size_list):
-        self._ship_list = []
-        for ship_size in ship_size_list:
-            self._ship_list.append(Ship(ship_size))
-        class Ship(object):
-            def __init__(self, size):
-                self._free_space = size
-                self._used_space = 0
-                self._good_type = None
-            def load_ship(self, good, number):
-                if (used_space == 0 or good == self._good_type) and number <= self._free_space:
-                    self._good_type = good
-                    self._free_space -= number
-                    self._used_space += number
-                    return number
-                return None
-            def unload_ship(self):
-                if self._free_space == 0:
-                    Supply.Barrels[self._good_type] += self._used_space
-                    self._free_space += self._used_space
-                    self._used_space = 0
-                return None
-
-class ColonistShip(object):
-    def __init__(self, colonists):
-        self._colonists_carried = 0
-        self._colonists_supply = colonists
-    def new_voyage(self, new_colonists):
-        if self._colonists_supply > new_colonists:
-            self._colonists_supply -= new_colonists
-            self._colonists_carried += new_colonists
-        else:
-            self._colonists_carried += self._colonists_supply
-            self._colonists_supply = 0
-            Market.endgame_trigger = True
-
-class Supply(object):
-    def __init__(self, barrel_dict, vp):
-        self._barrels = barrel_dict
-        self._vp = vp
+    class ColonistShip(object):
+        def __init__(self, colonists, number_of_players):
+            self.colonists_carried = number_of_players
+            self.colonists_supply = colonists - number_of_players
+    
+    class Supply(object):
+        def __init__(self, barrel_dict, vp):
+            self.barrels = barrel_dict
+            self.vp = vp
+    
     def check_vp_supply(self):
-        if self._vp <= 0:
-            Market.endgame_trigger = True
+        if self.supply.vp <= 0:
+            self.endgame_trigger = True
+
+    def empty_trading_queue(self):
+        while self.trading_post.trading_queue:
+            self.supply.barrels[self.trading_post.trading_queue.pop()] += 1
+        return None
+    
+    def new_colonist_voyage(self, new_colonists):
+        if self.colonist_ship.colonists_supply > new_colonists:
+            self.colonist_ship.colonists_supply -= new_colonists
+            self.colonist_ship.colonists_carried += new_colonists
+        else:
+            self.colonist_ship.colonists_carried += self.colonist_ship.colonists_supply
+            self.colonist_ship.colonists_supply = 0
+            self.endgame_trigger = True
--- market_flat_class.py
+++ market_flat_class.py
@@ -1,13 +1,40 @@
-import random
+class RoleOptions(object):
+    def __init__(self, prospectors):
+        self._role_list = [Role('mayor'), Role('captain'), Role('etc')]
+        self._role_list += (Role('prospector') * prospectors)
+        class Role(object):
+            def __init__(self, role_name):
+                self._role_name = role_name
+                self._stored_doubloons = 0
+                self._taken_flag = False
+    def choose_role(self, role):
+        if not self._role_list[role]._taken_flag:
+            Player.doubloons += self._role_list[role]._stored_doubloons
+            self._role_list[role]._stored_doubloons = 0
+            self._role_list[role]._.taken_flag = True
+            return Phase[role_name].full_phase()
+        return None
 
-class Market(object):
-    def __init__(self, colonists, vp, barrel_dict, plantation_list, quarries, building_dict, prospectors, ship_size_list):
-        self._endgame_trigger = False
-        self.building_supply = Buildings()
-        self._buildings = Buildings(building_dict)
-        self._plantations = Plantations(plantation_list, quarries)
-        self._role_options = RoleOptions(prospectors)
-        self._trading_post = TradingPost()
-        self._captain_ships = CaptainShips(ship_size_list)
-        self._colonist_ship = ColonistShip(colonists)
-        self._supply = Supply(barrel_dict, vp)
+class CaptainShips(object):
+    def __init__(self, ship_size_list):
+        self._ship_list = []
+        for ship_size in ship_size_list:
+            self._ship_list.append(Ship(ship_size))
+        class Ship(object):
+            def __init__(self, size):
+                self._free_space = size
+                self._used_space = 0
+                self._good_type = None
+            def load_ship(self, good, number):
+                if (used_space == 0 or good == self._good_type) and number <= self._free_space:
+                    self._good_type = good
+                    self._free_space -= number
+                    self._used_space += number
+                    return number
+                return None
+            def unload_ship(self):
+                if self._free_space == 0:
+                    Supply.Barrels[self._good_type] += self._used_space
+                    self._free_space += self._used_space
+                    self._used_space = 0
+                return None
