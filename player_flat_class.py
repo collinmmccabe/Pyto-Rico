e0219c5e7f2457d90795be79e118d0b99e0de487
Collin McCabe <collinmichaelmccabe@gmail.com>
2018-12-06 17:42:46 -0500

    Swap player and player_flat_class
    Use the latter for storage of old/working copies

--- player_flat_class.py
+++ player_flat_class.py
@@ -1,195 +1,90 @@
-class DictRef(dict):
-    def __getattr__(self, key):
-        return self[key]
-    def __setattr__(self, key, value):
-        self[key] = value
+# Need to figure out how to access/change parent class attributes...
 
-class Player(object):
-    def __init__(self, name, player_init_subdict):
-        self.name = name
-        self.doubloons = player_init_subdict['doubloons']
-        self.vp = 0
-        self.free_colonists = 0
-        self.governor_flag = player_init_subdict['governor']
-        self.island = self.Island(player_init_subdict['first_plantation'])
-        self.city = self.City()
-        self.supply = self.Barrels()
+class Player(name):
+    def __init__(self, name):
+        self._name = name
+        self._doubloons = 0
+        self._vp = 0
+        self._free_colonists = 0
+        self._governor_flag = False
+        self._island = Island()
+        self._city = City()
+        self._resources = Resources()
 
-    class Island(DictRef):
-        def __init__(self, first_plantation):
-            setattr(self, 'plot_01', self.Plantation(first_plantation))
-            for n in range(2, 13):
-                setattr(self, 'plot_' + str(n).zfill(2), None)  # i.e. self.plot_01 = Plantation()
+class Island():
+    def __init__(self):
+        self._plantation_list = []
+        # this is just here to remind me to remove it in other parts of the code
+        self._occupied_squares = len(self._plantation_list)
+    def build(self, plantation_type):
+        if len(self._plantation_list) < 12:
+            self._plantation_list.append(Plantation(plantation_type))
+        return None
+    def produce(self):
+        for good_type in ['corn', 'indigo', 'sugar', 'tobacco', 'coffee']:
+            taken = min(min(sum([1 for i in self._plantation_list if (i.good_produced == good_type and i.occupied_worker)]), 
+                            sum([j.occupied_workers for j in Player.City.building_list if j.good_processed == good_type])), 
+                        Market.Supply[good_type])
+            Market.Supply[good_type] -= taken
+            Player.Resources[good_type] += taken
+        return None
 
-        def swap_plots(self, from_plot, to_plot):
-            self[from_plot], self[to_plot] = self[to_plot], self[from_plot]
-            return None
-        def plots_built(self):
-            return sum(1 for i in self.values() if i is not None)
-        def build(self, island_plot, good_produced):
-            if ((self.plots_built() < 12) and (self[island_plot] is None)):
-                self[island_plot] = self.Plantation(good_produced)
-            return None
+class Plantation(good_type):
+    def __init__(self, good_type):
+        self._good_produced = good_type
+        self._occupied_worker = False
+    def add_colonist(self):
+        if (not self._occupied_worker) and Player.free_colonists > 0:
+            self._occupied_worker = True
+            Player.free_colonists -= 1
+        return None
+    def remove_colonist(self):
+        if self._occupied_worker:
+            self._occupied_worker = False
+            Player.free_colonists += 1
+        return None
 
-        class Plantation(DictRef):
-            def __init__(self, good_produced):
-                self.good_produced = good_produced
-                self.occupied = False
-    
-    class City(DictRef):
-        def __init__(self):
-            for n in range(1, 13):
-                setattr(self, 'plot_' + str(n).zfill(2), None)  # i.e. self.plot_01 = Building()
+class City():
+    def __init__(self):
+        self._building_list = []
+        self._occupied_squares = 0
+    def build(self, building_name):
+        if BuildingLookup[building_name].size + self._occupied_squares <= 12 and \
+          building_name not in self._building_list.building_name:  # self.building_list.building_name probably not right...
+            self._building_list.append(Building(building_name))
+            self._occupied_squares += BuildingLookup[building_name].size
+            if self._occupied_squares == 12:
+                Market.endgame_trigger = True
 
-        def swap_plots(self, from_plot, to_plot):
-            if ((self[from_plot] is not None) and (self[from_plot].size == 2)):
-                from_plot_plus_one = 'plot_' + str(int(from_plot[-2:]) + 1).zfill(2)
-                to_plot_plus_one = 'plot_' + str(int(to_plot[-2:]) + 1).zfill(2)
-                self.move_building(from_plot_plus_one, to_plot_plus_one)
-            self[from_plot], self[to_plot] = self[to_plot], self[from_plot]
-            return None
-        def plots_built(self):
-            return sum(1 for i in self.values() if i is not None)
-            
-        class Building(DictRef):
-            def __init__(self, building_lookup):
-                self.building_name = building_lookup.name
-                self.size = building_lookup.size
-                self.good_processed = building_lookup.good_processed
-                self.free_worker_spaces = building_lookup.free_worker_spaces
-                self.occupied_worker_spaces = 0
-    
-    class Barrels(DictRef):
-        def __init__(self):
-            self.corn = 0
-            self.indigo = 0
-            self.sugar = 0
-            self.tobacco = 0
-            self.coffee = 0
-    
-    def lose_doubloons(self, number):
-        if self.doubloons >= number:
-            self.doubloons -= number
-            return number
-        return None
-    def get_doubloons(self, number):
-        self.doubloons += number
-        return number
-    def get_vp(self, number):
-        self.vp += number
-        return number
-    def get_colonists(self, number):
-        self.free_colonists += number
-        return None
-    def set_governor_flag(self, bool_value):
-        self.governor_flag = bool_value
-        return None
-    
-    def add_colonist(self, region, plot):
-        if ((region == 'island') and (not self.island[plot].occupied) and (self.free_colonists > 0)):
-            self.island[plot].occupied = True
-            self.free_colonists -= 1
-        elif ((region == 'city') and (self.city[plot].free_worker_spaces > 0) and (self.free_colonists > 0)):
-            self.city[plot].occupied_worker_spaces += 1
-            self.city[plot].free_worker_spaces -= 1
-            self.free_colonists -= 1
-        return None
-    def remove_colonist(self, region, plot):
-        if ((region == 'island') and (self.island[plot].occupied)):
-            self.island[plot].occupied = False
-            self.free_colonists += 1
-        elif ((region == 'city') and (self.city[plot].occupied_worker_spaces > 0)):
-            self.city[plot].occupied_worker_spaces -= 1
-            self.city[plot].free_worker_spaces += 1
-            self.free_colonists += 1
-        return None
-    
-    def produce_plantation(self, market_supply_dict):  # send the market supply here to communicate with fn
-        taken = min(sum(1 for i in self.island if (i.good_produced == 'corn' and i.occupied)), market_supply_dict.corn)
-        market_supply_dict.corn -= taken
-        self.supply.corn += taken
-        for good_type in ['indigo', 'sugar', 'tobacco', 'coffee']:  # could replace list with keys of mkt spl d
-            taken = min(min(sum(1 for i in self.island if (i.good_produced == good_type and i.occupied)), 
-                            sum(j.occupied_worker_spaces for j in self.city if j.good_processed == good_type)), 
-                        market_supply_dict[good_type])
-            market_supply_dict[good_type] -= taken
-            self.supply[good_type] += taken
-        # Maybe use return value to communicate with Market class?
-        return market_supply_dict
-    
-    def build_building(self, city_plot, building_lookup, cost):  # specific building lookup is BuildingLookup[building_name]
-        endgame_trigger = False
-        if ((building_lookup.size + self.city.plots_built() <= 12) and (self.city[city_plot] is None) and
-          (building_lookup.name not in [i.building_name for i in self.city if i is not None]) and
-          (cost <= self.doubloons)):
-            if ((building_lookup.size == 2) and (self.city['plot_' + str(int(city_plot[-2:]) + 1).zfill(2)] is None)):
-                self.city['plot_' + str(int(city_plot[-2:]) + 1).zfill(2)] = self.city.Building({
-                                                                                'name': 'second plot for large building',
-                                                                                'size': 0,
-                                                                                'good_processed': None,
-                                                                                'free_worker_spaces': 0 })
-                self.city[city_plot] = self.city.Building(building_lookup)
-            elif building_lookup.size == 1:
-                self.city[city_plot] = self.city.Building(building_lookup)
-            else:
-                return endgame_trigger, None
-            if self.city.plots_built() == 12:
-                endgame_trigger = True
-            return endgame_trigger, self.lose_doubloons(cost)  # Use return value to communicate with Market class
-        return endgame_trigger, None
+class Building(building_name):
+    def __init__(self, building_name):
+        self._building_name = building_name
+        self._good_processed = BuildingLookup[building_name].worker_spaces
+        self._free_workers = BuildingLookup[building_name].worker_spaces
+        self._occupied_workers = 0
+    def add_colonist(self):
+        if self._free_workers > 0 and Player.free_colonists > 0:
+            self._occupied_workers += 1
+            self._free_workers -= 1
+            Player.free_colonists -= 1
+    def remove_colonist(self):
+        if self._occupied_workers > 0:
+            self._occupied_workers -= 1
+            self._free_workers += 1
+            Player.free_colonists += 1
 
-    def ship_resources(self, good, number):
-        if self.supply[good] >= number:
-            self.supply[good] -= number
-            return number, self.get_vp(number)  # this will be used to update Market.Supply[good] and Market.vp
-        return None, None
-    def trade_resources(self, good):
-        trade_value = {'corn': 0, 'indigo': 1, 'sugar': 2, 'tobacco': 3, 'coffee': 4}
-        if self.supply[good] > 0:
-            self.supply[good] -= 1
-            self.get_doubloons(trade_value[good])
-            return good  # for TradingPost.trading_queue.append(good)
-        return None
-    
-    def __repr__(self):
-        gov_desc = "not " if not self.governor_flag else ""
-        return ("\n{} is {}currently the governor.\n\nWEALTH:\n"
-                "doubloons: {}, victory points: {}, available colonists: {}\n"
-                "\nSUPPLY:\ncorn: {}, indigo: {}, sugar:{}, tobacco: {}, coffee: {}\n"
-                "\nISLAND:\nPlot 01: {}\nPlot 02: {}\nPlot 03: {}\nPlot 04: {}\nPlot 05: {}\nPlot 06: {}"
-                "\nPlot 07: {}\nPlot 08: {}\nPlot 09: {}\nPlot 10: {}\nPlot 11: {}\nPlot 12: {}\n"
-                "\nCITY:\nPlot 01: {}\nPlot 02: {}\nPlot 03: {}\nPlot 04: {}\nPlot 05: {}\nPlot 06: {}"
-                "\nPlot 07: {}\nPlot 08: {}\nPlot 09: {}\nPlot 10: {}\nPlot 11: {}\nPlot 12: {}\n".format(self.name,
-                                                                                  gov_desc,
-                                                                                  str(self.doubloons),
-                                                                                  str(self.vp),
-                                                                                  str(self.free_colonists),
-                                                                                  str(self.supply.corn),
-                                                                                  str(self.supply.indigo),
-                                                                                  str(self.supply.sugar),
-                                                                                  str(self.supply.tobacco),
-                                                                                  str(self.supply.coffee),
-                                                                                  self.island.plot_01,
-                                                                                  self.island.plot_02,
-                                                                                  self.island.plot_03,
-                                                                                  self.island.plot_04,
-                                                                                  self.island.plot_05,
-                                                                                  self.island.plot_06,
-                                                                                  self.island.plot_07,
-                                                                                  self.island.plot_08,
-                                                                                  self.island.plot_09,
-                                                                                  self.island.plot_10,
-                                                                                  self.island.plot_11,
-                                                                                  self.island.plot_12,
-                                                                                  self.city.plot_01,
-                                                                                  self.city.plot_02,
-                                                                                  self.city.plot_03,
-                                                                                  self.city.plot_04,
-                                                                                  self.city.plot_05,
-                                                                                  self.city.plot_06,
-                                                                                  self.city.plot_07,
-                                                                                  self.city.plot_08,
-                                                                                  self.city.plot_09,
-                                                                                  self.city.plot_10,
-                                                                                  self.city.plot_11,
-                                                                                  self.city.plot_12))
+class Resources():
+    def __init__(self):
+        self._corn = 0
+        self._indigo = 0
+        self._sugar = 0
+        self._tobacco = 0
+        self._coffee = 0
+    def shipping(self, good, number):
+        if self[good] >= number:
+            self[good] -= number
+            Market.Supply[good] += number
+    def trading(self, good):
+        if self[good] > 0:
+            self[good] -= 1
+            TradingPost.trading_queue.append(good)
